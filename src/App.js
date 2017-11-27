d6dbc71cf30d5ce4005a5ede8939f8c1521ba881
nithinlatha <latha@appinessworld.com>
2017-11-27 08:13:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,41 +7,44 @@ class App extends Component {
   constructor(props){
   super(props);
   this.state = {
-    key1: '',
-    key2 : '',
-    Key3 : ''
+    selectedValue : "",
+    appendValue : []
   };
-  this.changeState = this.changeState.bind(this);
-  this.changeState1 = this.changeState1.bind(this);
-  this.changeState2 = this.changeState2.bind(this);
-  this.multistate = this.multistate.bind(this);
-}
-changeState(value){
-  this.setState(
-    {key1 : 'A1,A2,A3'}
-  );
-}
-changeState1(value){
-  this.setState(
-    {key2 : 'B1,B2,B3'}
-  );
-}
-changeState2(value){
-  this.setState(
-    {key3 : 'C1,C2,C3'}
-  );
+  this.changeDropDown = this.changeDropDown.bind(this);
 }
 
-  multistate(value){
-    this.setState(
-      {
-        key4 : this.state.key1,
-        key5 : this.state.key1
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.state.selectedValue !=  nextState.selectedValue){
+      console.log(nextState.selectedValue);
+      switch(nextState.selectedValue) {
+        case "1" :
+        this.setState({
+          appendValue : ["A1","A2","A3"]
+        });
+        break;
+        case "2" :
+        this.setState({
+          appendValue : ["B1","B2","B#"]
+        });
+        break;
+        case "3" :
+        this.setState({
+          appendValue : ["C1","C2","C3"]
+        });
+        break;
       }
-    );
+    }
+    return true;
+  }
+
+  changeDropDown(e){
+    this.setState({
+      selectedValue : e.target.value
+    });
   }
 
   render() {
+    console.log(this.state.appendValue);
     return (
       <div className="App">
         <header className="App-header">
@@ -50,21 +53,18 @@ changeState2(value){
         </header>
         <Row>
         <h2>shfsjdfh</h2>
-        <DropdownButton  title="Dropdown">
-           <MenuItem eventKey="1" onSelect={this.changeState} active>A</MenuItem>
-           <MenuItem eventKey="2"  onSelect={this.changeState1}>B</MenuItem>
-           <MenuItem eventKey="3" onSelect={this.changeState2}>C</MenuItem>
-         </DropdownButton>
-         <DropdownButton  title="Dropdown1">
-           <MenuItem eventKey="1" onSelect={this.multistate} >{this.state.key1}</MenuItem>
-           <MenuItem eventKey="2" >{this.state.key2}</MenuItem>
-           <MenuItem eventKey="3" >{this.state.key3}</MenuItem>
-          </DropdownButton>
-          <DropdownButton  title="Dropdown2">
-             <MenuItem eventKey="1">{this.state.key4},{this.state.key5}</MenuItem>
-             <MenuItem eventKey="2">{this.state.key2}</MenuItem>
-             <MenuItem eventKey="3" >{this.state.key3}</MenuItem>
-           </DropdownButton>
+        <select onChange={this.changeDropDown} className="form-control">
+         <option value={1}>A</option>
+         <option value={2}>B</option>
+         <option value={3}>C</option>
+       </select>
+        <select  className="form-control" >
+          {
+            this.state.appendValue.map((value,index)=>{
+              return <option key={index} value={index}>{value}</option>;
+            })
+          }
+        </select>
         </Row>
       </div>
     );
